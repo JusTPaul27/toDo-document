94d3e4b2ced1bfc1ebbdbc29724ad2bf5f51180d
Paolo Foppiano <103486420+JusTPaul27@users.noreply.github.com>
2022-05-05 13:19:03 +0200

    push delle 13.18

--- model.js
+++ model.js
@@ -1,35 +1,116 @@
 class ToDo {
 
     static PRIORITY = {
-        Low: 0,
-        Medium: 1,
-        High: 2,
-        veryHigh: 3,
+        Low: { order: 0, name: 'Bassa', color: 'green' },
+        Medium: { order: 1, name: 'Media', color: 'yellow' },
+        High: { order: 2, name: 'Alta', color: 'Orange' },
+        veryHigh: { order: 3, name: 'Molto alta', color: 'red' },
     }
 
-    constructor(name, priority, tags = []) {
+    constructor(name, priority = ToDo.PRIORITY.Low, tags = []) {
         this.name = name;
-        this.priority = priority;
+        this._priority = priority;
         this.tags = tags;
+        this._creationDate = new Date().getTime();
     }
+
+    set priority(newPriority) {
+        this._priority = newPriority;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    set creationDate(date) {
+        const timeStamp = date.getTime();
+        this._creationDate = timeStamp;
+    }
+
+    get creationDate() {
+        const date = new Date(this._creationDate);
+        return date
+    }
+
+
 
     toString() {
         const todoString = 'ToDo: ' + this.name + '\n' +
-            'Priority: ' + ToDo.fromPriorityToString(this.priority) + '\n' +
-            'Tags: ' + this.tags;
+            'Priority: ' + this.priority.name + '\n' +
+            'Tags: ' + this.tags + '\n' +
+            'Creation date: ' + this.creationDate;
 
         return todoString;
     }
 
-    static fromPriorityToString(selectedPriority) {
-        if (selectedPriority === 0) {
-            return 'Low';
-        } else if (selectedPriority === 1) {
-            return 'Medium';
-        } else if (selectedPriority === 2) {
-            return 'High';
+    // static fromPriorityToString(selectedPriority) {
+    //     if (selectedPriority === 0) {
+    //         return 'Low';
+    //     } else if (selectedPriority === 1) {
+    //         return 'Medium';
+    //     } else if (selectedPriority === 2) {
+    //         return 'High';
+    //     } else {
+    //         return 'Very High';
+    //     }
+    // }
+}
+
+
+class DeadLineToDo extends ToDo {
+
+    constructor(name, deadLineDate = null, priority = ToDo.PRIORITY.Low, tags = []) {
+        super(name, priority, tags);
+        if (deadLineDate === null) {
+            this._deadLineDate = this._creationDate + (1000 * 60 * 60 * 24);
+            //     const tomorrow = new Date(this._creationDate.getTime());
+            //     tomorrow.setDate(tomorrow.getDate() + 1);
+            //     this._deadLineDate = tomorrow.getTime();
         } else {
-            return 'Very High';
+            this._deadLineDate = deadLineDate.getTime();
         }
     }
-}
+
+    get priority() {
+        const nowTimeStamp = new Date().getTime();
+        const deltaTime = this._deadLineDate - nowTimeStamp;
+        const day = 1000 * 60 * 60 * 24;
+        let deadLinePriority;
+        if (deltaTime <= day) {
+            deadLinePriority = ToDo.PRIORITY.veryHigh;
+        } else if (deltaTime <= 2 * day) {
+            deadLinePriority = ToDo.PRIORITY.High;
+        } else  if (deltaTime <= 3 * day) {
+            deadLinePriority =  ToDo.PRIORITY.Medium;
+        } else {
+             deadLinePriority =  ToDo.PRIORITY.Low
+        }
+
+        if (this._priority.order > deadLinePriority.order) {
+            return this._priority;
+        } else {
+            return deadLinePriority;
+        }
+    }
+
+    set deadLineDate(date) {
+            const timeStamp = date.getTime();
+            this._deadLineDate = timeStamp;
+        }
+
+    get deadLineDate() {
+            const date = new Date(this._deadLineDate);
+            return date
+        }
+
+        toString(){
+            const todoString = super.toString() + '\n' +
+                'Deadline: ' + this.deadLineDate;
+
+            return todoString
+        }
+
+    }
+
+
+    
--- script.js
+++ script.js
@@ -1,5 +1,71 @@
 
 
-const pippo = new ToDo('Compra il tabacco', ToDo.PRIORITY.Medium, ['Droga', ' No ske agente']);
+const task1 = new ToDo('Compra il latte ', ToDo.PRIORITY.Medium, ['spesa', 'frigo']);
 
-console.log(pippo.toString());
+const task2 = new ToDo('Compra la focaccia', ToDo.PRIORITY.Low, ['spesa', 'casa']);
+
+const task3 = new DeadLineToDo('Fai gli auguri a Nonna', new Date(2022, 6, 9), ToDo.PRIORITY.High, ['famiglia', 'compleanni']);
+
+const task4 = new DeadLineToDo('Chiama Marco');
+
+const task5 = new DeadLineToDo('Vai a allenamento', null, ToDo.PRIORITY.High, ['Salute']);
+
+
+const toDoList = [task1, task2, task3, task4, task5];
+
+function displayToDo(toDoList) {
+
+    const container = document.getElementById('todo-container');
+
+    for (let i = 0; i < toDoList.length; i++) {
+        const todo = toDoList[i];
+
+        const par = document.createElement('p');
+
+        par.classList.add('list-element');
+
+        const node = document.createTextNode(todo.name);
+
+        const creationDateNode = document.createTextNode(todo.creationDate);
+
+        const priorityNode = document.createTextNode(" Priority " + todo._priority.name ) ;
+
+        const tagsNode = document.createTextNode(todo.tags);
+
+        const deadLineDateNode = document.createTextNode(' Da completare enro il: ' + todo.deadLineDate);
+
+        const br = document.createElement('br')
+
+        par.appendChild(node);
+
+        par.appendChild(br);
+
+        par.appendChild(tagsNode);
+
+        par.appendChild(br);
+
+        par.appendChild(creationDateNode);
+
+        par.appendChild(deadLineDateNode);
+      
+        par.appendChild(br);
+
+        par.appendChild(priorityNode);
+
+        container.appendChild(par);
+
+    }
+}
+
+displayToDo(toDoList)
+
+
+
+
+
+
+
+
+
+
+const doneList = [];
